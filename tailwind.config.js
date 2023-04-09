1c91f711906d7c3b4bd14100f5ce2fc48eeb7061
AnorakDWise <gregory442005@gmail.com>
2023-04-10 00:48:21 +0200

    Front card view init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontSize: {
+        title: "2rem",
+        subtitle: "1.75rem",
+        ordinary: "1.5rem",
       },
     },
   },
   plugins: [],
-}
+};
